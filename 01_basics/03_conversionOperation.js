9bf9cd8f0aaf10affee8f44f190da1400164e855
CHETAN <142914310+ChetanChauhan1997@users.noreply.github.com>
2025-06-05 17:40:14 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+"use strict";
+
+let score = null;
+ 
+// console.log(typeof score); 
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //Number 
+// console.log(valueInNumber); //NaN
+
+// Number Conversion 
+// "33" => 33
+// "33abce" => NaN  (Not a Number)
+// ture/false => 1/0
+// undefined => NaN
+// null => 0
+
+
+let isLoggedIn = 22;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// Boolean Conversion
+
+// 1 => true  // 0 => false
+// "shiva" => true
+// "" => false
+// null => false
+// undefined => false
+
+
+let someNumber = undefined;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// String Conversion
+// 33 => "33"
+// true = "true"
+// null = "null"
+// undefined = "undefined"
+
+
+
+
+
+
+
+
+
+
+
