d98cb6e3f1721151e68429d6cd0f3345453febfb
CHETAN <142914310+ChetanChauhan1997@users.noreply.github.com>
2025-06-06 18:35:01 +0000

    new chapter add

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);  //f
+// console.log(null == 0);  //f
+// console.log(null >= 0);  //t
+
+//the reason is that an equality check == and comparisons >, <, >=, <= work differently.
+// Comparisons convert null to a number, treating it as 0.
+// that's why (3) null >= 0 is ture and (1)null > 0 is false
+
+// console.log(undefined == 0);  // f
+// console.log(undefined > 0);  // f
+// console.log(undefined < 0);  // f
+
+
+// strict Check
+//  ===
+// console.log("2" === 2);  // f
+
+
+
