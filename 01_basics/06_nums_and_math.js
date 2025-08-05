da139d67c010dcbb5048055d01a5aa2b48fb56ec
CHETAN <142914310+ChetanChauhan1997@users.noreply.github.com>
2025-08-05 16:10:47 +0000

    math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,60 @@
+const score = 400;
+
+const balance = new Number(100)
+// console.log( score);
+// console.log(typeof score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNum = 123.8966;
+
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());  // default USA 
+// console.log(hundreds.toLocaleString('en-IN'));  // Default INDIA
+
+
+/********************Maths*************** */
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));   // it will always take high value
+// console.log(Math.floor(4.9));  // it will always take low value
+
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(4,2));
+
+// console.log(Math.min(4,2,3,33,4,1));
+// console.log(Math.max(4,2,5,7,4));
+
+
+// console.log(Math.random());  //it always give value between 0 to 1 
+
+// console.log((Math.random()*10) + 1); 
+// console.log(Math.round((Math.random()*10) + 1)); 
+
+const min = 10 ;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
